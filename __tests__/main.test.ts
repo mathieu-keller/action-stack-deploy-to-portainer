12fd24173a6edeee7e83341d2358ab3fff1a5cfb
Dat Nguyen <datntdev@outlook.com>
2023-10-27 23:01:17 +0700

    test: write unit tests for below implementation

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,80 +1,60 @@
-/**
- * Unit tests for the action's main functionality, src/main.ts
- *
- * These should be run as if the action was called from a workflow.
- * Specifically, the inputs listed in `action.yml` should be set as environment
- * variables following the pattern `INPUT_<INPUT_NAME>`.
- */
-
 import * as core from '@actions/core'
-import * as main from '../src/main'
+import { run } from '../src/main'
+import { Stack } from '../src/models'
+import PortainerApiInstance from '../src/portainer-api'
 
-// Mock the GitHub Actions core library
-const debugMock = jest.spyOn(core, 'debug')
-const getInputMock = jest.spyOn(core, 'getInput')
-const setFailedMock = jest.spyOn(core, 'setFailed')
-const setOutputMock = jest.spyOn(core, 'setOutput')
+jest.mock('@actions/core')
+jest.mock('../src/portainer-api')
 
-// Mock the action's main function
-const runMock = jest.spyOn(main, 'run')
+describe('run', () => {
+  const mockGetInput = jest.spyOn(core, 'getInput')
+  const mockSetFailed = jest.spyOn(core, 'setFailed')
+  const mockGetStackListAsync = jest.fn()
+  const mockDeleteStackAsync = jest.fn()
+  const mockPostStandaloneStackFromFile = jest.fn()
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const parameters: any = {
+    portainerHost: 'mock host',
+    portainerApiKey: 'mock api key',
+    portainerEnvId: 'mock env id',
+    portainerStackName: 'mock stack name',
+    portainerFilePath: 'mock file path',
+    portainerEnvVars: 'mock env vars'
+  }
 
-describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
+
+    mockGetInput.mockImplementation(name => parameters[name])
+    ;(PortainerApiInstance as jest.Mock).mockImplementation(() => ({
+      getStackListAsync: mockGetStackListAsync,
+      deleteStackAsync: mockDeleteStackAsync,
+      postStandaloneStackFromFile: mockPostStandaloneStackFromFile
+    }))
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return '500'
-        default:
-          return ''
-      }
-    })
+  it('should get parameters, get stack list, delete stack, and post standalone stack', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const mockStacks: Stack[] = [{ Id: 123, Name: 'mock stack name' }]
+    mockGetStackListAsync.mockResolvedValueOnce(mockStacks)
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+    await run()
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
+    expect(mockGetInput).toHaveBeenCalledTimes(6)
+    expect(PortainerApiInstance).toHaveBeenCalledWith(parameters)
+    expect(mockGetStackListAsync).toHaveBeenCalledTimes(1)
+    expect(mockDeleteStackAsync).toHaveBeenCalledWith(123)
+    expect(mockPostStandaloneStackFromFile).toHaveBeenCalledTimes(1)
   })
 
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
-      }
-    })
+  it('should set failed when an error occurs', async () => {
+    const error = new Error('mock error')
+    mockGetStackListAsync.mockRejectedValueOnce(error)
 
-    await main.run()
-    expect(runMock).toHaveReturned()
+    await run()
 
-    // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(
-      1,
-      'milliseconds not a number'
-    )
+    expect(mockSetFailed).toHaveBeenCalledWith(error.message)
   })
 })
---
+++ __tests__/portainer-api.test.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import PortainerApiInstance from '../src/portainer-api'
+import { Stack } from '../src/models'
+import { Parameters } from '../src/parameters'
+import * as utils from '../src/utils'
+
+jest.mock('../src/utils')
+
+describe('PortainerApiInstance', () => {
+  const parameters: Parameters = {
+    portainerHost: 'mock host',
+    portainerApiKey: 'mock api key',
+    portainerEnvId: 'mock env id',
+    portainerStackName: 'mock stack name',
+    portainerFilePath: 'mock file path',
+    portainerEnvVars: 'mock env vars'
+  }
+
+  const mockFetch = jest.fn()
+  global.fetch = mockFetch
+
+  const mockGetFileBlob = jest.spyOn(utils, 'getFileBlob')
+  mockGetFileBlob.mockReturnValue(new Blob(['mock file data']))
+
+  const instance = new PortainerApiInstance(parameters)
+
+  it('should get stack list', async () => {
+    // @ts-ignore
+    const mockStacks: Stack[] = [{}]
+    mockFetch.mockResolvedValueOnce({
+      json: async () => Promise.resolve(mockStacks)
+    })
+
+    const stacks = await instance.getStackListAsync()
+
+    expect(stacks).toEqual(mockStacks)
+    expect(mockFetch).toHaveBeenCalledWith(
+      `${parameters.portainerHost}/api/stacks?filters={"EndpointID":2,"IncludeOrphanedStacks":true}`,
+      expect.anything()
+    )
+  })
+
+  it('should delete stack', async () => {
+    mockFetch.mockResolvedValueOnce({ ok: true })
+
+    await instance.deleteStackAsync(123)
+
+    expect(mockFetch).toHaveBeenCalledWith(
+      `${parameters.portainerHost}/api/stacks/123?endpointId=${parameters.portainerEnvId}`,
+      expect.objectContaining({ method: 'DELETE' })
+    )
+  })
+
+  it('should post standalone stack from file', async () => {
+    // @ts-ignore
+    const mockStack: Stack = {}
+    mockFetch.mockResolvedValueOnce({
+      json: async () => Promise.resolve(mockStack)
+    })
+
+    const stack = await instance.postStandaloneStackFromFile()
+
+    expect(stack).toEqual(mockStack)
+    expect(mockFetch).toHaveBeenCalledWith(
+      `${parameters.portainerHost}/api/stacks/create/standalone/file?endpointId=${parameters.portainerEnvId}`,
+      expect.objectContaining({ method: 'POST' })
+    )
+    expect(mockGetFileBlob).toHaveBeenCalledWith(parameters.portainerFilePath)
+  })
+})
